b5fddc5a85d8a6f6021017d2aa60def4d25362e3
Talal <nweirant@gmail.com>
2019-01-15 19:07:53 -0500

    ready to deploy

--- proxy/index.js
+++ proxy/index.js
@@ -9,23 +9,23 @@ app.use(bodyParser.json());
 
 app.use("/:id", express.static(path.join(__dirname, "../views")));
 
-app.use(
-  "/api/carousel",
-  proxy({ target: "http://ec2-18-223-116-251.us-east-2.compute.amazonaws.com/" })
-);
-app.use(
-  "/api/description",
-  proxy({
-    target: "http://ec2-18-218-251-40.us-east-2.compute.amazonaws.com:3009/"
-  })
-);
+// app.use(
+//   "/api/carousel",
+//   proxy({ target: "http://ec2-18-223-116-251.us-east-2.compute.amazonaws.com/" })
+// );
+// app.use(
+//   "/api/description",
+//   proxy({
+//     target: "http://ec2-18-218-251-40.us-east-2.compute.amazonaws.com:3009/"
+//   })
+// );
 app.use(
   "/api/details",
-  proxy({ target: "http://ec2-18-218-63-198.us-east-2.compute.amazonaws.com/" })
+  proxy({ target: "http://ec2-18-188-219-64.us-east-2.compute.amazonaws.com/" })
 );
 app.use(
   "/api/similarlistings",
-  proxy({ target: "http://ec2-54-174-166-132.compute-1.amazonaws.com/" })
+  proxy({ target: "http://ec2-18-206-61-102.compute-1.amazonaws.com/" })
 );
 
 app.listen(3000, () => console.log("Proxy Server Working!!!"));
