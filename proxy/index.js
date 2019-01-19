288153ae62e730f1257cda4528954d8dcfd94cf4
Talal <nweirant@gmail.com>
2019-01-19 11:22:01 -0500

    updated for new urls and another component

--- proxy/index.js
+++ proxy/index.js
@@ -9,23 +9,18 @@ app.use(bodyParser.json());
 
 app.use("/:id", express.static(path.join(__dirname, "../views")));
 
-// app.use(
-//   "/api/carousel",
-//   proxy({ target: "http://ec2-18-223-116-251.us-east-2.compute.amazonaws.com/" })
-// );
-// app.use(
-//   "/api/description",
-//   proxy({
-//     target: "http://ec2-18-218-251-40.us-east-2.compute.amazonaws.com:3009/"
-//   })
-// );
+app.use(
+  "/api/carousel",
+  proxy({ target: "http://18.188.188.228:3010/" })
+);
+
 app.use(
   "/api/details",
   proxy({ target: "http://ec2-18-188-219-64.us-east-2.compute.amazonaws.com/" })
 );
 app.use(
   "/api/similarlistings",
-  proxy({ target: "http://ec2-3-86-84-148.compute-1.amazonaws.com/" })
+  proxy({ target: "http://54.82.218.221/" })
 );
 
 app.listen(3000, () => console.log("Proxy Server Working!!!"));
